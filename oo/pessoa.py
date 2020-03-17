aa001e7f0e995853e76da30e2600c3d08ae8908e
Anderson-Alberto <correa.andersonalberto@gmail.com>
2020-03-17 18:14:42 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa("Anderson")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print (p.nome)
-    p.nome = 'Andeus'
-    print (p.nome)
-    print(p.idade)
+    alberto = Pessoa(nome="Alberto ")
+    anderson = Pessoa(alberto, nome = "Anderson")
+    print(Pessoa.cumprimentar(anderson))
+    print(id(anderson))
+    print(anderson.cumprimentar())
+    print (anderson.nome)
+    print(anderson.idade)
+    for filho in anderson.filhos:
+        print(filho.nome)
